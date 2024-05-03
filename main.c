a3df01ec4e712feb1ab4116159aad2502c233149
davy <dss10191@gmail.com>
2024-05-03 10:30:00 -0300

    feat: basic project

---
+++ main.c
@@ -0,0 +1,207 @@
+/**********************************************************
+* @brief    Trabalho trimestral - Resta Um
+* @author   Marcos Zuccolotto, Davi Schuch
+* @date     abr/2024
+* @version  1.1
+*
+* Fundacao Liberato - Curso de Eletronica
+* Elementos de programacao  - prof. Marcos Zuccolotto
+*
+* Trabalho trimestral - Struct / enum / ponteiros
+*
+* Implemente as funcoes abaixo comforme especificacao,
+* respeitando os parâmetros e tipos de dados definidos,
+* seguindo as orientacoes dadas
+* - qualJogada()
+* - movimenta()
+* - confereJogo()
+*
+* Equipe : Bancada (2 alunos)
+**********************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <locale.h>
+#include "restaUm.h"
+
+#define MAXMSG 100  // tamanho maximo de mensagens
+
+void inicTab(char* tab) {
+    for (int i = 0; i < NLIN; i++) {
+        for (int j = 0; j < NCOL; j++) {
+            *(tab + i * NLIN + j) = OC;
+
+            if (i == 0 || i == 1 || i == 5 || i == 6) {
+                if (j == 0 || j == 1 || j == 5 || j == 6) {
+                    *(tab + i * NLIN + j) = NU;
+                }
+            }
+        }
+    }
+    *(tab + 3 * NLIN + 3) = VZ;
+}
+
+void showTab(char* tab) {
+    printf("  0 1 2 3 4 5 6");
+    printf("\n");
+    for (int i = 0; i < NLIN; i++) {
+        printf("%i ", i);
+        for (int j = 0; j < NCOL; j++) {
+            printf("%c ", * (tab + i * NLIN + j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
+
+status_t qualJogada(movimento_t* jog) {
+    char s;
+    do {
+        printf("Deseja continuar?\n");
+        printf("Sim (S) - Nao (N): ");
+        scanf_s(" %c", &s);
+    } while (s != 's' && s != 'S' && s != 'n' && s != 'N');
+    switch (s) {
+        case 's':
+        case 'S':
+            printf("Insira as coordenadas da peça que deseja mover\n");
+            printf("Linha: ");
+            scanf_s("%d", &jog->origem.lin);
+            printf("Coluna: ");
+            scanf_s("%d", &jog->origem.col);
+            printf("Insira as coordenadas do destino da peça\n");
+            printf("Linha: ");
+            scanf_s("%d", &jog->destino.lin);
+            printf("Coluna: ");
+            scanf_s("%d", &jog->destino.col);
+
+            return OK;
+        case 'n':
+        case 'N':
+            return DERROTA;
+    }
+}
+
+status_t movimenta(char* tab, movimento_t* jog) {
+    int oLin, oCol, dLin, dCol, dfLin, dfCol;
+    char d, o;
+    oLin = *(&jog->origem.lin);
+    oCol = *(&jog->origem.col);
+    dLin = *(&jog->destino.lin);
+    dCol = *(&jog->destino.col);
+    dfLin = oLin - dLin;
+    dfCol = oCol - dCol;
+    o = *(tab + oLin * NLIN + oCol);
+    d = *(tab + dLin * NLIN + dCol);
+
+    if (oLin == dLin && (dfCol != -2 && dfCol != 2)) {
+        return INVALIDO;
+    } else if (oCol == dCol && (dfLin != -2 && dfLin != 2)) {
+        return INVALIDO;
+    } else if (d == VZ && o == OC && (oLin == dLin || oCol == dCol)) {
+        *(tab + oLin * NLIN + oCol) = VZ;
+        *(tab + dLin * NLIN + dCol) = OC;
+        if (oLin == dLin) {
+            if (oCol > dCol) {
+                *(tab + oLin * NLIN + (oCol - 1)) = VZ;
+            } else {
+                *(tab + oLin * NLIN + (oCol + 1)) = VZ;
+            }
+        } else {
+            if (oLin > dLin) {
+                *(tab + (oLin-1) * NLIN + oCol) = VZ;
+            }
+            else {
+                *(tab + (oLin + 1) * NLIN + oCol) = VZ;
+            }
+        }
+        return OK;
+    } else if (o == VZ) {
+        return VAZIO;
+    } else if (d == OC) {
+        return OCUPADO;
+    } else {
+        return INVALIDO;
+    }
+}
+
+status_t confereJogo(char* tab) {
+    int qtdblank = 0, qtdpieces = 0, qtdfail = 0;
+    
+    for (int i = 0; i < NLIN; i++) {
+        for (int j = 0; j < NCOL; j++) {
+            switch (*(tab + i * NLIN + j)) {
+                case VZ:
+                    qtdblank++;
+                    break;
+                case OC:
+                    qtdpieces++;
+
+                    if (*(tab + (i - 1) * 7 + j) == VZ && *(tab + (i + 1) * 7 + j) == VZ && *(tab + i * 7 + (j - 1)) == VZ && *(tab + i * 7 + (j + 1)) == VZ) {
+                        qtdfail++;
+                    }
+
+                    break;
+            }
+        }
+    }
+
+    if (qtdblank == 32 && qtdpieces == 1) {
+        return VITORIA;
+    } else if (qtdfail == qtdpieces) {
+        return DERROTA;
+    } else {
+        return OK;
+    }
+}
+
+int main() {
+    // Variaveis
+    char tabRestaUm[NLIN][NCOL]; // tabuleiro
+    status_t estado = OK;        // resultado da função
+    movimento_t jogada;          // movimento a realizar
+    char msg[MAXMSG];            // mensagem para usuario
+    unsigned int numJogadas = 0; // numero de jogadas realizadas
+
+    // Inicializacao
+    setlocale(LC_ALL, "");       // caracteres da lingua portuguesa
+    inicTab(&tabRestaUm);        // preenche tabuleiro inicial
+    showTab(&tabRestaUm);        // exibe o tabulero
+
+    // Laco principal
+    do {
+        estado = qualJogada(&jogada);
+        if (estado != DERROTA) { // se não pedir para sair, segue o jogo
+            estado = movimenta(&tabRestaUm, &jogada);
+            switch (estado) {
+                case INVALIDO:
+                    strncpy_s(msg, MAXMSG, "Movimento inválido\n", MAXMSG);
+                    break;
+                case VAZIO:
+                    strncpy_s(msg, MAXMSG, "Sem peças a mover\n", MAXMSG);
+                    break;
+                case OCUPADO:
+                    strncpy_s(msg, MAXMSG, "Posição destino ocupada\n", MAXMSG);
+                    break;
+
+                default:
+                    showTab(&tabRestaUm);
+                    numJogadas++;
+                    estado = confereJogo(&tabRestaUm);
+                    switch (estado) {
+                        case VITORIA:
+                            strncpy_s(msg, MAXMSG, "Parabéns, sobrou só um!\n", MAXMSG);
+                            break;
+                        case DERROTA:
+                            strncpy_s(msg, MAXMSG, "Acabaram as jogadas!\n", MAXMSG);
+                            break;
+                        default:
+                            strncpy_s(msg, MAXMSG, "Jogo continua...\n", MAXMSG);
+                    }
+            }
+            printf(msg);
+        } // fim if derrota
+
+    } while (estado != VITORIA && estado != DERROTA);
+    printf("Fim de jogo com %d rodadas!\n", numJogadas);
+}// fim main
---
+++ restaUm.h
@@ -0,0 +1,86 @@
+/**********************************************************
+* @file     restaUm.h
+* @brief    Resta Um - defines, tipos dados e prototipos
+* @author   Marcos Zuccolotto, Davi Schuch
+* @date     abr/2024
+* @version  1.1
+**********************************************************/
+#ifndef _RESTA_UM_H_   // Declaracao de guarda
+#define _RESTA_UM_H_
+
+// Dimensoes da matriz que guarda o tabuleiro
+#define NCOL 7
+#define NLIN 7
+
+// Marcadores do tabuleiro
+#define  NU ' '  // Ponto invalido/proibido
+#define  OC '*'  // Ponto ocupado
+#define  VZ '_'  // Ponto livre/vazio
+
+/* Tipos de dados */
+// Posicao da peca no tabuleiro
+
+typedef struct {
+    int lin; // Posicao linha
+    int col; // Posicao destino
+} posTab_t;
+
+// Movimento a ser realizado
+typedef struct {
+    posTab_t origem;   // Posicao inicio/origem
+    posTab_t destino; // Posicao fim
+} movimento_t;
+
+typedef enum {
+    OK = 0,   // 0 - movimento executado, segue o jogo
+    INVALIDO, // 1 - não existe peca a pular sobre/retira/fora tabuleiro/movimento longo, curto ou em diagonal
+    OCUPADO,  // 2 - posicao destino ocupada
+    VAZIO,    // 3 - posicao origem vazia
+    VITORIA,  // 4 - vitoria - fim jogo
+    DERROTA   // 5 - nao tem mais o que fazer
+} status_t;
+
+/* Prototipos */
+/*
+* @brief  Inicializa tabuleiro
+* @param  tabuleiro
+* @retval none
+*/
+void inicTab(char* tab);
+
+/*
+* @brief  Exibe o tabuleiro
+* @param  tabuleiro
+* @retval none
+*/
+void showTab(char* tab);
+
+/**
+* @brief  Entrada dados da jogada
+* @param  movimentacao a realizar
+* @retval OK - jogador definiu a movimentacao
+          DERROTA - jogador desistiu do jogo
+*/
+status_t qualJogada(movimento_t* jog);
+
+/*
+* @brief  Executa a movimentacao solicitada
+* @param  tabuleiro
+* @param  movimentacao a realizar
+* @retval OK - movimento executado, segue o jogo
+          INVALIDO - movimento invalido
+          OCUPADO - posicao destino ocupada
+          VAZIO - posicao origem vazia
+*/
+status_t movimenta(char* tab, movimento_t* jog);
+
+/*
+* @brief  Confere possibilidades de continuacao
+* @param  tabuleiro
+* @retval OK - segue o jogo
+          VITORIA - restou um, acabou
+          DERROTA - nada mais a fazer, fim de jogo
+*/
+status_t confereJogo(char* tab);
+
+#endif //_RESTA_UM_H_
