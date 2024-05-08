e4d668b4b4e79b4c2e3b485fa93bb521b35dc6d1
davy <dss10191@gmail.com>
2024-05-08 19:24:42 -0300

    refactor: send main functions to header file (professor request)

--- restaUm.h
+++ restaUm.h
@@ -46,14 +46,38 @@ typedef enum {
 * @param  tabuleiro
 * @retval none
 */
-void inicTab(char* tab);
+void inicTab(char* tab) {
+    for (int i = 0; i < NLIN; i++) {
+        for (int j = 0; j < NCOL; j++) {
+            *(tab + i * NLIN + j) = OC;
+
+            if (i == 0 || i == 1 || i == 5 || i == 6) {
+                if (j == 0 || j == 1 || j == 5 || j == 6) {
+                    *(tab + i * NLIN + j) = NU;
+                }
+            }
+        }
+    }
+    *(tab + 3 * NLIN + 3) = VZ;
+}
 
 /*
 * @brief  Exibe o tabuleiro
 * @param  tabuleiro
 * @retval none
 */
-void showTab(char* tab);
+void showTab(char* tab) {
+    printf("  0 1 2 3 4 5 6");
+    printf("\n");
+    for (int i = 0; i < NLIN; i++) {
+        printf("%i ", i);
+        for (int j = 0; j < NCOL; j++) {
+            printf("%c ", *(tab + i * NLIN + j));
+        }
+        printf("\n");
+    }
+    printf("\n");
+}
 
 /**
 * @brief  Entrada dados da jogada
@@ -61,7 +85,33 @@ void showTab(char* tab);
 * @retval OK - jogador definiu a movimentacao
           DERROTA - jogador desistiu do jogo
 */
-status_t qualJogada(movimento_t* jog);
+status_t qualJogada(movimento_t* jog) {
+    char s;
+    do {
+        printf("Deseja continuar?\n");
+        printf("Sim (S) - Nao (N): ");
+        scanf_s(" %c", &s);
+    } while (s != 's' && s != 'S' && s != 'n' && s != 'N');
+    switch (s) {
+    case 's':
+    case 'S':
+        printf("Insira as coordenadas da peça que deseja mover\n");
+        printf("Linha: ");
+        scanf_s("%d", &jog->origem.lin);
+        printf("Coluna: ");
+        scanf_s("%d", &jog->origem.col);
+        printf("Insira as coordenadas do destino da peça\n");
+        printf("Linha: ");
+        scanf_s("%d", &jog->destino.lin);
+        printf("Coluna: ");
+        scanf_s("%d", &jog->destino.col);
+
+        return OK;
+    case 'n':
+    case 'N':
+        return DERROTA;
+    }
+}
 
 /*
 * @brief  Executa a movimentacao solicitada
@@ -72,7 +122,74 @@ status_t qualJogada(movimento_t* jog);
           OCUPADO - posicao destino ocupada
           VAZIO - posicao origem vazia
 */
-status_t movimenta(char* tab, movimento_t* jog);
+status_t movimenta(char* tab, movimento_t* jog) {
+    int oLin, oCol, dLin, dCol, dfLin, dfCol;
+    char d, o;
+    oLin = *(&jog->origem.lin);
+    oCol = *(&jog->origem.col);
+    dLin = *(&jog->destino.lin);
+    dCol = *(&jog->destino.col);
+    dfLin = oLin - dLin;
+    dfCol = oCol - dCol;
+    o = *(tab + oLin * NLIN + oCol);
+    d = *(tab + dLin * NLIN + dCol);
+    if (oLin == dLin && (dfCol != -2 && dfCol != 2)) {
+        return INVALIDO;
+    }
+    else if (oCol == dCol && (dfLin != -2 && dfLin != 2)) {
+        return INVALIDO;
+    }
+    else if (d == VZ && o == OC && (oLin == dLin || oCol == dCol)) {
+        if (oLin == dLin) {
+            if (oCol > dCol) {
+                if (*(tab + oLin * NLIN + (oCol - 1)) != VZ && *(tab + oLin * NLIN + (oCol - 1)) != NU) {
+                    *(tab + oLin * NLIN + (oCol - 1)) = VZ;
+                }
+                else {
+                    return INVALIDO;
+                }
+            }
+            else {
+                if (*(tab + oLin * NLIN + (oCol + 1)) != VZ && *(tab + oLin * NLIN + (oCol + 1)) != NU) {
+                    *(tab + oLin * NLIN + (oCol + 1)) = VZ;
+                }
+                else {
+                    return INVALIDO;
+                }
+            }
+        }
+        else {
+            if (oLin > dLin) {
+                if (*(tab + (oLin - 1) * NLIN + oCol) != VZ && *(tab + (oLin - 1) * NLIN + oCol) != NU) {
+                    *(tab + (oLin - 1) * NLIN + oCol) = VZ;
+                }
+                else {
+                    return INVALIDO;
+                }
+            }
+            else {
+                if (*(tab + (oLin + 1) * NLIN + oCol) != VZ && *(tab + (oLin + 1) * NLIN + oCol) != NU) {
+                    *(tab + (oLin + 1) * NLIN + oCol) = VZ;
+                }
+                else {
+                    return INVALIDO;
+                }
+            }
+        }
+        *(tab + oLin * NLIN + oCol) = VZ;
+        *(tab + dLin * NLIN + dCol) = OC;
+        return OK;
+    }
+    else if (o == VZ) {
+        return VAZIO;
+    }
+    else if (d == OC) {
+        return OCUPADO;
+    }
+    else {
+        return INVALIDO;
+    }
+}
 
 /*
 * @brief  Confere possibilidades de continuacao
@@ -81,6 +198,57 @@ status_t movimenta(char* tab, movimento_t* jog);
           VITORIA - restou um, acabou
           DERROTA - nada mais a fazer, fim de jogo
 */
-status_t confereJogo(char* tab);
+status_t confereJogo(char* tab) {
+    int qtdblank = 0, qtdpieces = 0, qtdfail = 0;
+
+    for (int i = 0; i < NLIN; i++) {
+        for (int j = 0; j < NCOL; j++) {
+            switch (*(tab + i * NLIN + j)) {
+            case VZ:
+                qtdblank++;
+                break;
+            case OC:
+                qtdpieces++;
+
+                char up = *(tab + (i - 1) * 7 + j),
+                    down = *(tab + (i + 1) * 7 + j),
+                    left = *(tab + i * 7 + (j - 1)),
+                    right = *(tab + i * 7 + (j + 1));
+
+
+                if ((up == VZ || up == NU) && (down == VZ || down == NU) && (left == VZ || left == NU) && (right == VZ || right == NU)) {
+                    qtdfail++;
+                    break;
+                }
+                else if (i == 0 && down == VZ) {
+                    qtdfail++;
+                    break;
+                }
+                else if (i == 6 && up == VZ) {
+                    qtdfail++;
+                    break;
+                }
+                else if (j == 0 && left == VZ) {
+                    qtdfail++;
+                    break;
+                }
+                else if (j == 6 && right == VZ) {
+                    qtdfail++;
+                    break;
+                }
+            }
+        }
+    }
+
+    if (qtdblank == 32 && qtdpieces == 1) {
+        return VITORIA;
+    }
+    else if (qtdfail == qtdpieces) {
+        return DERROTA;
+    }
+    else {
+        return OK;
+    }
+}
 
 #endif //_RESTA_UM_H_
